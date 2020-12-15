6ad9b6f9077743ce70759085ba4ec876b425983f
ChanakaVrtsa <chanakaroshane8332@gmail.com>
2020-12-15 14:08:00 +0530

    First Commit

---
+++ src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args){
+        
+        //Checked Exceptions Example
+        //AccountManager class handles all account related functions
+        AccountManager accountMananger = new AccountManager();
+        System.out.println("Checked Exception");
+        try {
+            //calls verifyAccount method
+            accountMananger.VerifyAccount(121121);
+        } catch (AccountManagerException msg) {
+            //prints all exceptions occured during propagation
+            msg.printStackTrace();
+        }
+
+        //Unchecked Exceptions Example
+        System.out.println("Unchecked Exception");
+        try {
+            //calls withdrawMonney method of AccountManager
+            accountMananger.withdrawMoney(100);
+        } catch (Exception e) {
+            //prints the stack trace if execption is caught
+            e.printStackTrace();
+        }
+        
+        
+    }
+}
